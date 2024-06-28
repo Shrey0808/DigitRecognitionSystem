1f5cfe4c77a57b307cfc5941b4a982b421ccff58
Shrey0808 <104291716+Shrey0808@users.noreply.github.com>
2024-06-29 00:47:14 +0530

    Add files via upload

--- deloyment/app.py
+++ deloyment/app.py
@@ -1,45 +1,48 @@
-# from prediction import preprocess_and_extract_digits
-# import matplotlib.pyplot as plt
-
-# image_path = 'static\img1.jpg' 
-# predictions, digit = preprocess_and_extract_digits(image_path)
-# print("Predictions:", predictions)
-
-
-
-
-from flask import Flask ,render_template, url_for, request , jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from prediction import preprocess_and_extract_digits
+import os
+import cv2
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+# Folder to save uploaded images
+UPLOAD_FOLDER = 'static/uploads'
+PROCESSED_FOLDER = 'static/processed'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(img_path)
+            predictions, images = preprocess_and_extract_digits(img_path)
+            
+            processed_filenames = []
+            for idx, img in enumerate(images):
+                processed_filename = f"processed_{idx}.png"
+                processed_path = os.path.join(app.config['PROCESSED_FOLDER'], processed_filename)
+                cv2.imwrite(processed_path, img)
+                processed_filenames.append(processed_filename)
+            
+            return render_template('index.html', uploaded_filename=filename, processed_filenames=processed_filenames, predictions=predictions)
     return render_template('index.html')
 
-# POST
-# @app.route('/predict',methods = ['GET','POST'])
-# def predict():
-#     if request.method =='POST':
-#         n = int(request.form['nitrogen'])
-#         p = int(request.form['phosphorus'])
-#         k = int(request.form['potassium'])
-#         t = float(request.form['temperature'])
-#         h = float(request.form['humidity'])
-#         ph = float(request.form['ph'])
-#         r = float(request.form['rainfall'])
-#         user_data = [[n , p , k , t, h, ph , r ]]
-#         trans_data = scaler.transform(user_data)
-#         prediction = kmeans.predict(trans_data)[0]
-#         print(prediction)
-#         dt = dict(df[df['cluster_12'] == prediction]['label'].value_counts())
-#         ls = []
-#         for k,v in dt.items():
-#             if v>=70:
-#                 ls.append(k)
-#         print(ls)
-#         return jsonify(ls)
+app.jinja_env.globals.update(zip=zip)
 
-
-
-if __name__ =="__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
